7075e28275ebcb6e3c4b3b169f51f393706bea29
AzureGear <celestial.azura@gmail.com>
2024-07-08 22:42:09 +0300

    Attributes

--- test.py
+++ test.py
@@ -1,4 +1,212 @@
 # ----------------------------------------------------------------------------------------------------------------------
+from PyQt5.QtCore import Qt, pyqtSlot, pyqtProperty
+from PyQt5.QtGui import QPainter, QColor, QPen, QBrush
+from PyQt5.QtWidgets import QApplication, QWidget, QSlider, QPushButton, QCheckBox, QSpinBox, QColorDialog, \
+    QHBoxLayout, QVBoxLayout
+
+
+class RoundProgressbar(QWidget):
+    def __init__(
+            self,
+            parent=None,
+            color: QColor = QColor(170, 0, 255),
+            size: int = 100,
+            thickness: int = 10,
+            value: int = 24,
+            maximum: int = 100,
+            round_edge: bool = False,
+            bg_circle_color: QColor = QColor(0, 0, 0, 0),
+            fill_bg_circle: bool = False
+    ):
+        if parent is not None:
+            super().__init__(parent=parent)
+        elif parent is None:
+            super().__init__()
+        self._circular_size = size
+        self._thickness = thickness
+        self.resize(self._circular_size + (self._thickness * 2), self._circular_size + (self._thickness * 2))
+        self._color = color
+        self._maximum = maximum
+        self._value = value
+        self._alen = (self._value / self._maximum) * 360
+        self._a = -(self._alen - 90)
+        self._round_edge = round_edge
+        self._bg_circle_color = bg_circle_color
+        self._fill_bg_circle = fill_bg_circle
+
+    def paintEvent(self, paint_event):
+        painter = QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QPainter.Antialiasing)
+        painter.setPen(QPen(self._bg_circle_color, self._thickness - 1, Qt.SolidLine))
+        if self._fill_bg_circle:
+            painter.setBrush(QBrush(self._bg_circle_color, Qt.SolidPattern))
+        elif not self._fill_bg_circle:
+            painter.setBrush(Qt.NoBrush)
+        painter.drawEllipse(self._thickness, self._thickness, self._circular_size, self._circular_size)
+        if self._round_edge:
+            painter.setPen(QPen(self._color, self._thickness, Qt.SolidLine, Qt.RoundCap))
+        elif not self._round_edge:
+            painter.setPen(QPen(self._color, self._thickness, Qt.SolidLine, Qt.FlatCap))
+        painter.setBrush(Qt.NoBrush)
+        painter.drawArc(
+            self._thickness,
+            self._thickness,
+            self._circular_size,
+            self._circular_size,
+            self._a * 16,
+            self._alen * 16
+        )
+        painter.end()
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self._circular_size = (self.width() - (self._thickness * 2)) if self.width() < self.height() else (
+                self.height() - (self._thickness * 2))
+
+    def get_value(self):
+        return self._value
+
+    @pyqtSlot(int)
+    def set_value(self, value: int):
+        self._value = value
+        self._alen = (self._value / self._maximum) * 360
+        self._a = -(self._alen - 90)
+        self.update()
+
+    value = pyqtProperty(int, get_value, set_value)
+
+    def get_maximum(self):
+        return self._maximum
+
+    @pyqtSlot(int)
+    def set_maximum(self, value: int):
+        self._maximum = value
+        self._alen = (self._value / self._maximum) * 360
+        self._a = -(self._alen - 90)
+        self.update()
+
+    maximum = pyqtProperty(int, get_maximum, set_maximum)
+
+    def get_thickness(self):
+        return self._thickness
+
+    @pyqtSlot(int)
+    def set_thickness(self, value: int):
+        self._thickness = value
+        self._circular_size = (self.width() - (self._thickness * 2)) if self.width() < self.height() else (
+                self.height() - (self._thickness * 2))
+        self.update()
+
+    thickness = pyqtProperty(int, get_thickness, set_thickness)
+
+    def get_color(self):
+        return self._color
+
+    @pyqtSlot(QColor)
+    def set_color(self, color: QColor):
+        self._color = color
+        self.update()
+
+    color = pyqtProperty(QColor, get_color, set_color)
+
+    def get_bg_circle_color(self):
+        return self._bg_circle_color
+
+    @pyqtSlot(QColor)
+    def set_bg_circle_color(self, color: QColor):
+        self._bg_circle_color = color
+        self.update()
+
+    background_circle_color = pyqtProperty(QColor, get_bg_circle_color, set_bg_circle_color)
+
+    def get_round_edge(self):
+        return self._round_edge
+
+    @pyqtSlot(bool)
+    def set_round_edge(self, value: bool):
+        self._round_edge = value
+        self.update()
+
+    round_edge = pyqtProperty(bool, get_round_edge, set_round_edge)
+
+    def get_fill_bg_circle(self):
+        return self._fill_bg_circle
+
+    @pyqtSlot(bool)
+    def set_fill_bg_circle(self, value: bool):
+        self._fill_bg_circle = value
+        self.update()
+
+    fill_background_circle = pyqtProperty(bool, get_fill_bg_circle, set_fill_bg_circle)
+
+
+class Form(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.init_ui()
+
+    def init_ui(self):
+        self.resize(600, 400)
+        self.setWindowTitle("Round Progress Bar")
+        self.progressbar = RoundProgressbar()
+        self.progressbar.resize(120, 120)
+        self.slider = QSlider(Qt.Horizontal)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(100)
+        self.slider.setValue(self.progressbar.get_value())
+        self.slider.setToolTip("This slider sets the progress bar value")
+        self.slider.valueChanged.connect(lambda: self.progressbar.set_value(self.slider.value()))
+        self.thickness_spinbox = QSpinBox()
+        self.thickness_spinbox.setValue(self.progressbar.thickness)
+        self.thickness_spinbox.setToolTip("This number defines the progress bar thickness")
+        self.thickness_spinbox.valueChanged.connect(
+            lambda: self.progressbar.set_thickness(self.thickness_spinbox.value()))
+        self.round_edge_checkbox = QCheckBox("Round edge")
+        self.round_edge_checkbox.setToolTip("Leaving this option checked, will make the progress bar edges round")
+        self.round_edge_checkbox.clicked.connect(lambda: self.progressbar.set_round_edge(
+            True) if self.round_edge_checkbox.isChecked() else self.progressbar.set_round_edge(False))
+        self.change_color_button = QPushButton("Change color")
+        self.change_color_button.setToolTip("Select a color for the progress bar")
+        self.change_color_button.clicked.connect(lambda: self.open_dialog(self.progressbar, 0))
+        self.change_bg_circle_color_button = QPushButton("Change background circle color")
+        self.change_bg_circle_color_button.setToolTip("Select a color for the progress bar background")
+        self.change_bg_circle_color_button.clicked.connect(lambda: self.open_dialog(self.progressbar, 1))
+        self.fill_bg_circle_checkbox = QCheckBox("Fill background circle center")
+        self.fill_bg_circle_checkbox.setToolTip(
+            "Leaving this option checked, will fill the background of the progress bar"
+        )
+        self.fill_bg_circle_checkbox.clicked.connect(lambda: self.progressbar.set_fill_bg_circle(
+            True) if self.fill_bg_circle_checkbox.isChecked() else self.progressbar.set_fill_bg_circle(False))
+        self.vertical_box = QVBoxLayout()
+        self.vertical_box.addWidget(self.progressbar)
+        self.vertical_box.addWidget(self.slider)
+        self.vertical_box.addWidget(self.thickness_spinbox)
+        self.vertical_box.addWidget(self.round_edge_checkbox)
+        self.vertical_box.addWidget(self.change_color_button)
+        self.vertical_box.addWidget(self.change_bg_circle_color_button)
+        self.vertical_box.addWidget(self.fill_bg_circle_checkbox)
+        self.main_horizontal_box = QHBoxLayout()
+        self.main_horizontal_box.addLayout(self.vertical_box)
+        self.setLayout(self.main_horizontal_box)
+        self.show()
+
+    def open_dialog(self, widget, color_index):
+        color = QColorDialog.getColor(
+            initial=widget._color if color_index == 0 else widget._bg_circle_color,
+            options=QColorDialog.ShowAlphaChannel
+        )
+        if color.isValid():
+            if color_index == 0:
+                widget.set_color(color)
+            elif color_index == 1:
+                widget.set_bg_circle_color(color)
+
+
+import sys
+app = QApplication(sys.argv)
+form = Form()
+sys.exit(app.exec_())
 
 # ----------------------------------------------------------------------------------------------------------------------
 
